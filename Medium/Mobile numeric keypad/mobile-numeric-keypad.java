da2d88b012393d11ec7ae09bf407b6c733b73bb6
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-06-15 09:23:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Mobile numeric keypad/mobile-numeric-keypad.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.getCount(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // keypad layout
+    private static final int[][] keyPad = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        {-1, 0, -1}
+    };
+    
+    // Possible movements: right, left, down, up
+    private static final int[] dirX = {0, 0, 1, -1};
+    private static final int[] dirY = {1, -1, 0, 0};
+    
+    public long getCount(int N){
+        if(N == 1) return 10;
+        
+        long[] dpPrevious = new long[10];
+        long[] dpCurrent = new long[10];
+        
+        for(int i=0;i<10;i++){
+            dpPrevious[i] = 1;
+        }
+        
+        for(int length = 2;length <= N;length++){
+            for(int i=0;i<4;i++){
+                for(int j=0;j<3;j++){
+                    if(keyPad[i][j] != -1){
+                        int key = keyPad[i][j];
+                        dpCurrent[key] = dpPrevious[key];
+                        for(int k=0;k<4;k++){
+                            int newI = i + dirX[k];
+                            int newJ = j + dirY[k];
+                            if(newI >= 0 && newJ >= 0 && newI < 4 && newJ < 3 && keyPad[newI][newJ] != -1){
+                                dpCurrent[key] += dpPrevious[keyPad[newI][newJ]];
+                            }
+                        }
+                    }
+                }
+            }
+            long[] temp = dpPrevious;
+            dpPrevious = dpCurrent;
+            dpCurrent = temp;
+        }
+        long ans = 0;
+        for(int i=0;i<10;i++){
+            ans += dpPrevious[i];
+        }
+        return ans;
+    }
+}
